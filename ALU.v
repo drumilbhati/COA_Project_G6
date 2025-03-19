b6c35ebf04b528643800e6735c1e1ac5e774036d
Drumil Bhati <drumilbhati5@gmail.com>
2025-03-19 14:16:50 +0530

    Add ALU module and implement 32-to-1 multiplexer

--- ALU.v
+++ ALU.v
@@ -1,3 +1,9 @@
 module ALU(clk, Reset, Imm7, Op1, Op2, OpCode, ALU_Save, ZFlag_Save, CFlag_Save);
+    input clk, Reset, Imm7;
+    input [4:0] OpCode;
+    input [7:0] Op1, Op2;
+    output wire ZFlag_Save, CFlag_Save;
+    output wire [7:0] ALU_Save;
 
+    
 endmodule
