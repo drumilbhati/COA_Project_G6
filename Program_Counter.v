8783da6813dcb3b61e1a106db78d5ad55f4ccb0a
Devarsh Doshi <devarsh.d2@ahduni.edu.in>
2025-03-28 21:50:26 +0530

    Inst_Mem with ROM and 25 bit Register

--- Program_Counter.v
+++ Program_Counter.v
@@ -1,4 +1,4 @@
-module Program_Counter(Enable_PC, Update_PC, clk, New_Address, PC, PC_D2);
+module Program_Counter(Enable_PC,Reset, Update_PC, clk, New_Address, PC, PC_D2);
 input Enable_PC, Update_PC, clk;
 input[7:0] New_Address;
 output[7:0] PC;
@@ -10,8 +10,8 @@ Mux_2to1 inst1(8'b00000000,8'b00000001,Enable_PC,mux1_out);
 ripple_carry_adder inst2(mux1_out,PC, 1'b0,adder_out,cout,c7);
 wire[7:0] mux2_out;
 Mux_2to1 inst3(adder_out,New_Address,Update_PC,mux2_out);
-RegisterSynW inst4(clk,1'b0,1'b1,mux2_out,PC);
+RegisterSynW inst4(clk,Reset,1'b1,mux2_out,PC);
 wire[7:0] PC_D1;
-RegisterSynW inst5(clk,1'b0,1'b1,PC,PC_D1);
-RegisterSynW inst6(clk,1'b0,1'b1,PC_D1,PC_D2);
+RegisterSynW inst5(clk,Reset,1'b1,PC,PC_D1);
+RegisterSynW inst6(clk,Reset,1'b1,PC_D1,PC_D2);
 endmodule
