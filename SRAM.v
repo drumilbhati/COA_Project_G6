dd95767d7f80ac890c88e9108cc76a2764e3ac9b
Prasham Mehta <prashammehta360@gmail.com>
2025-04-04 13:17:05 +0530

    Final_Commit

--- SRAM.v
+++ SRAM.v
@@ -1,34 +1,32 @@
 module SRAM (
     input clk,                 
-    input Reset,                // Synchronous Reset
-    input [7:0] Address,        // 8-bit Address input
-    input SRAMRead,             // Read Enable
-    input SRAMWrite,            // Write Enable
-    input [7:0] Datain,         // 8-bit Data input
-    output reg [7:0] Dataout    // 8-bit Data output
+    input Reset,               
+    input [7:0] Address,        
+    input SRAMRead,             
+    input SRAMWrite,            
+    input [7:0] Datain,         
+    output reg [7:0] Dataout    
 );
 
-    // Declaring 256 x 8-bit memory array
     reg [7:0] datamem [0:255];
 
-    // Synchronous reset (clears memory)
+
     integer i;
     always @(posedge clk) begin
         if (Reset) begin
             for (i = 0; i < 256; i = i + 1)
-                datamem[i] <= 8'b00000000; // Clearing memory on reset
+                datamem[i] <= 8'b00000000; 
         end 
         else if (SRAMWrite) begin
-            datamem[Address] <= Datain; // Writing operation
+            datamem[Address] <= Datain; 
         end
     end
 
-    // Read operation (Combinational)
     always @(*) begin
         if (SRAMRead)
-            Dataout = datamem[Address]; // Reading from memory
+            Dataout = datamem[Address]; 
         else
-            Dataout = 8'b00000000; // Giving output as 0 when not reading
+            Dataout = 8'b00000000; 
     end
 
 endmodule
